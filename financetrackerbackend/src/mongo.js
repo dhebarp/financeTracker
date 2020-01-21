ab2e1bc059b96118d2b9afe0da3c94ec59206726
dhebarp <paritoh.dhebar@gmail.com>
2020-01-21 11:34:25 +1100

    mortgageSchema created

---
+++ financetrackerbackend/src/mongo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://localhost/${process.env.MONGO_COLLECTION}` || "financetracker", {useNewURLParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+    //we're connected!
+    console.log("Mongoose online")
+         });
