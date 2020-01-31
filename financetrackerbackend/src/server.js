87a4d12803bb3ea860a2fc793d453b803140b7d7
dhebarp <paritoh.dhebar@gmail.com>
2020-02-01 10:49:18 +1100

    port chnaged again thnx callan

--- financetrackerbackend/src/server.js
+++ financetrackerbackend/src/server.js
@@ -26,7 +26,7 @@ app.use('/mortgage', mortgageApi);
 app.use('/cashflow', cashflowApi);
 
 
-const port = process.env.EXPRESS_PORT || 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log("port" + port);
 });
