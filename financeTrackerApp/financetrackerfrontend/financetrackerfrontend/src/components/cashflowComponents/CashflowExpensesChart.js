ede9cbbca597fc9dc162f96d42ad00e4256c19fb
Paritosh Dhebar <git config --global user.email paritosh.dhebar@gmail.com>
2020-02-15 03:28:26 +1100

    cashflow graphs are dynamic and will updated based on monthly data

--- financeTrackerApp/financetrackerfrontend/financetrackerfrontend/src/components/cashflowComponents/CashflowExpensesChart.js
+++ financeTrackerApp/financetrackerfrontend/financetrackerfrontend/src/components/cashflowComponents/CashflowExpensesChart.js
@@ -1,18 +1,40 @@
-import React, { useLayoutEffect } from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import React from 'react';
+import { Doughnut, Bar, Pie } from 'react-chartjs-2';
 
 class Chart extends React.Component {
-     
+
     render() {
         return (
-            <div className={'container'}>
-                <Doughnut
-                    data={this.props.chartData}
-                    width={300}
-                    height={300}
-                    options={{ responsive: true, maintainAspectRatio: false, legend:{position: "right"}}}
-                />
-            </div>
+            <React.Fragment>
+                <div className={'container'}>
+                    <div class="row">
+                        <div class="col">
+                            <Doughnut
+                                data={this.props.doughnutData}
+                                width={200}
+                                height={200}
+                                options={{ responsive: true, maintainAspectRatio: false, legend: { position: "right" } }}
+                            />
+                        </div>
+                        <div class="col">
+                            <Bar
+                                data={this.props.barChartData}
+                                width={200}
+                                height={200}
+                                options={{ responsive: true, maintainAspectRatio: false, legend: { position: "top" } }}
+                            />
+                        </div>
+                        <div class="col">
+                            <Pie
+                                data={this.props.pieChartData}
+                                width={200}
+                                height={200}
+                                options={{ responsive: true, maintainAspectRatio: false, legend: { position: "top" } }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
--- financeTrackerApp/financetrackerfrontend/financetrackerfrontend/src/components/cashflowComponents/CashflowManager.js
+++ financeTrackerApp/financetrackerfrontend/financetrackerfrontend/src/components/cashflowComponents/CashflowManager.js
@@ -6,27 +6,22 @@ export function CashflowManager() {
 
   const [incomes, setIncomes] = useState({});
   const [expenses, setExpenses] = useState([]);
-  const [category] = useState(categories.slice(1, 12));
+  const [category, setCategory] = useState(categories.slice(1, 12));
+  // add calculations to state here
   const [month, setMonth] = useState('');
-  const [chartData, setChartData] = useState({
+  const [doughnut, setDoughnut] = useState({});
+  const [pie, setPie] = useState({})
+  const [bar, setBar] = useState({
     labels: category, //labels need to match data
     datasets: [{
       data: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], //hardcoded values for now.
-      backgroundColor: [
-        'rgba(115, 24, 111, 0.9)',
-        'rgba(37, 42, 17, 0.6)',
-        'rgba(182, 158, 125, 0.8)',
-        'rgba(183, 164, 179, 0.8)',
-        'rgba(137, 105, 111, 0.6)',
-        'rgba(22, 128, 92, 0.5)',
-        'rgba(253, 69, 43, 0.5',
-        'rgba(113, 200, 49, 0.8)',
-        'rgba(253, 149, 36, 0.8)',
-        'rgba(145, 232, 131, 0.7)',
-        'rgba(5, 214, 225, 1)'
-      ]
+      backgroundColor: ['rgba(115, 24, 111, 0.9)','rgba(37, 42, 17, 0.6)','rgba(182, 158, 125, 0.8)','rgba(183, 164, 179, 0.8)',
+        'rgba(137, 105, 111, 0.6)','rgba(22, 128, 92, 0.5)','rgba(253, 69, 43, 0.5','rgba(113, 200, 49, 0.8)','rgba(253, 149, 36, 0.8)',
+        'rgba(145, 232, 131, 0.7)','rgba(5, 214, 225, 1)']
     }]
   });
+
+
 
   useEffect(() => {
     fetch(`/cashflow/view/${month}`,
@@ -36,9 +31,26 @@ export function CashflowManager() {
       })
       .then(async data => {
         const newData = await data.json();
-        console.log(newData);
-        setIncomes(newData[0].incomes)
-        setExpenses(newData[0].expenses)
+        setIncomes(newData[0].incomes);
+        setExpenses(newData[0].expenses);
+        // setCategory(newData[0].expenses);
+        setPie({
+          labels: ["Primary Income", "Investment Income", "Other Income"], //labels need to match data
+          datasets: [{
+            data: Object.values(newData[0].incomes), //hardcoded values for now.
+            backgroundColor: ['rgba(115, 24, 111, 0.9)','rgba(37, 42, 17, 0.6)','rgba(182, 158, 125, 0.8)']
+          }]
+        });
+        const updatedExpenses = newData[0].expenses.slice(1, 100).map(a => a.amount);
+        const updatedCategoryList = newData[0].expenses.slice(1, 100).map(a => a.category);
+
+        setDoughnut({
+          labels: updatedCategoryList, //labels need to match data
+          datasets: [{
+            data: updatedExpenses,
+            backgroundColor: ['rgba(115, 24, 111, 0.9)','rgba(37, 42, 17, 0.6)','rgba(182, 158, 125, 0.8)','rgba(183, 164, 179, 0.8)','rgba(137, 105, 111, 0.6)','rgba(22, 128, 92, 0.5)','rgba(253, 69, 43, 0.5','rgba(113, 200, 49, 0.8)','rgba(253, 149, 36, 0.8)','rgba(145, 232, 131, 0.7)','rgba(5, 214, 225, 1)']
+          }]
+        })
       }).catch(err => {
         console.log(err);
       })
@@ -47,6 +59,7 @@ export function CashflowManager() {
 
   return (
     <React.Fragment>
+      
       <div className="container">
         <div className="input-group input-group-sm mb-3">
           <select className="custom-select" value={month} onChange={(event) => setMonth(event.target.value)} >
@@ -56,7 +69,7 @@ export function CashflowManager() {
           </select>
         </div>
       </div>
-      <Chart chartData={chartData} />
+      <Chart doughnutData={doughnut} barChartData={bar} pieChartData={pie}/>
       <br />
       <div className="container">
         <table className="table table-hover">
